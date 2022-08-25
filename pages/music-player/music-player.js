6312952503bc44b78f60ceb26b343ea1f41a07f6
chenyq <1123159757@qq.com>
2022-08-25 15:46:26 +0800

    代码重构 抽取到store

--- pages/music-player/music-player.js
+++ pages/music-player/music-player.js
@@ -1,34 +1,35 @@
 // pages/music-player/music-player.js
-import { getSongDetail, getSongLyric } from "../../services/player"
-import { parseLyric } from "../../utils/parse-lyric"
-import playStore from "../../storge/playStore"
+import playStore, { audioContext } from "../../storge/playStore"
 import throttle from "../../utils/throttle"
 
 const app = getApp()
-const audioContext = wx.createInnerAudioContext()
 const modeNames = ["order", "repeat", "random"]
 
 Page({
   data: {
+    stateKeys: ["id", "currentSong", "currentTime", "durationTime", "lyricInfos", "currentLyricText", "currentLyricIndex", "sliderValue", "isPlaying", "playModeIndex"],
+
     id: 0,
     currentSong: {},
+    currentTime: 0,
+    durationTime: 0,
     lyricInfos: [],
+    currentLyricText: "",
+    currentLyricIndex: -1,
+
+    isPlaying: true,
+    isFirstPlay: true,
+
+    playSongList: [],
+    playSongIndex: 0,
+    playModeName: "order",
+
     currentPage: 0,
     contentHeight: 0,
     pageTitles: [ "歌曲", "歌词" ],
-
-    currentTime: 0,
-    durationTime: 0,
     sliderValue: 0,
-    isPlaying: true,
-    currentLyricText: "",
-    currentLyricIndex: -1,
-    lyricScrollTop: 0,
-    playSongList: [],
-    playSongIndex: 0,
-    isFirstPlay: true,
-    playModeIndex: 0,
-    playModeName: "order"
+    
+    lyricScrollTop: 0
   },
   onLoad(options) {
     // 0.获取设备信息
@@ -39,96 +40,13 @@ Page({
     // 1.获取歌曲的id
     const id = options.id
     // 根据id播放歌曲
-    this.setupPlaySong(id)
+    playStore.dispatch("playMusicWithSong", id)
 
     // 5.获取store中的共享数据
     playStore.onStates(["playSongList", "playSongIndex"], this.getPlaySongInfosHandler)
+    playStore.onStates(this.data.stateKeys, this.getPlayerInfosHandler)
   },
 
-  // ---------------------发送网络请求---------------------
-  async fetchSongDetail() {
-    const res = await getSongDetail(this.data.id)
-    this.setData({ 
-      currentSong: res.songs[0],
-      durationTime: res.songs[0].dt
-    })
-  },
-  async fetchSongLyric() {
-    const res = await getSongLyric(this.data.id)
-    const lyricString = res.lrc.lyric;
-    const lyricInfos = parseLyric(lyricString)
-    this.setData({ lyricInfos })
-  },
-  // ---------------------封装函数相关---------------------
-  // 封装监听歌曲实时播放进度的函数
-  timeUpDate() {
-    audioContext.onTimeUpdate(() => {
-      // 设置当前时间
-      this.setData({ currentTime: audioContext.currentTime * 1000 })
-
-      // 设置滑块进度
-      const sliderValue = this.data.currentTime / this.data.durationTime * 100
-      this.setData({ sliderValue })
-
-      // 匹配正确歌词
-      if (!this.data.lyricInfos.length) return
-      let index = this.data.lyricInfos.length - 1
-      for (let i = 0; i < this.data.lyricInfos.length; i++) {
-        const info = this.data.lyricInfos[i]
-        if (info.time > audioContext.currentTime * 1000) {
-          index = i - 1
-          break
-        }
-      }
-
-      // 获取歌词的索引和文本, 并且改变歌词滚动页面的位置
-      if (this.data.currentLyricIndex === index) return
-      this.setData({ 
-        currentLyricText: this.data.lyricInfos[index].text,
-        currentLyricIndex: index,
-        lyricScrollTop: 35 * index
-      })
-    })
-  },
-  // 封装播放歌曲核心逻辑
-  setupPlaySong(id) {
-    // 保存id
-    this.setData({ id })
-
-    // 2.1根据id获取歌曲的详细数据
-    this.fetchSongDetail()
-    // 2.2根据id获取歌曲的歌词数据
-    this.fetchSongLyric()
-
-    // 3.播放当前的歌曲
-    audioContext.stop()
-    audioContext.src = `https://music.163.com/song/media/outer/url?id=${id}.mp3`
-    audioContext.autoplay = true
-
-    // 第一次进入才需要监听
-    if (this.data.isFirstPlay) {
-      this.setData({ isFirstPlay: false })
-
-      // 4.监听歌曲播放进度
-      // 4.1调用监听播放进度的函数
-      this.timeUpDate()
-
-      // 4.2当歌曲没有缓存, 正在加载时, 暂停播放
-      audioContext.onWaiting(() => {
-        audioContext.pause()
-      })
-
-      // 4.3当歌曲缓存完成时, 继续播放
-      audioContext.onCanplay(() => {
-        audioContext.play()
-      })
-
-      // 4.4监听歌曲播放完成
-      audioContext.onEnded(() => {
-        this.changeNewSong()
-      })
-    }
-  },
 
   // ---------------------事件监听函数---------------------
   // 轮播图改变
@@ -147,7 +65,7 @@ Page({
   // 滑块拖动完成的监听
   onSliderChange(event) {
     // 拖动完成, 恢复歌曲进度的监听
-    this.timeUpDate()
+    playStore.dispatch("timeUpDate")
     // 展示播放的图标(解决歌曲播放显式暂停图标)
     this.setData({ isPlaying: true })
 
@@ -171,70 +89,28 @@ Page({
   }, 100),
   // 监听播放暂停
   onPlayOrPause() {
-    const isPlaying = audioContext.paused
-    this.setData({ isPlaying })
-    if (isPlaying) {
-      audioContext.play()
-    } else {
-      audioContext.pause()
-    }
+    playStore.dispatch("changeMusicStatus")
   },
   // 上一首按钮
   onPrevtBtnTap() {
     // 展示播放的图标(解决歌曲播放显式暂停图标)
     this.setData({ isPlaying: true })
 
-    this.changeNewSong(false)
+    playStore.dispatch("playNewMusic", false)
   },
   // 下一首按钮
   onNextBtnTap() {
     // 展示播放的图标(解决歌曲播放显式暂停图标)
     this.setData({ isPlaying: true })
 
-    this.changeNewSong()
-  },
-  // 控制上一首或下一首封装的函数
-  changeNewSong(isNext = true) {
-    // 获取之前的数据
-    let index = this.data.playSongIndex
-    const length = this.data.playSongList.length
-
-    // 根据不同的模式, 将索引重新赋值
-    switch (this.data.playModeIndex) {
-      case 0: // 顺序播放
-        index = isNext ? index + 1 : index - 1
-        if (index === length) index = 0
-        if (index === -1) index = length - 1
-        break;
-      case 1: // 单曲循环
-        break;
-      case 2: // 随机播放
-        index = Math.floor(Math.random() * length)
-        break;
-    }    
-
-    // 根据索引获取歌曲
-    const newSong = this.data.playSongList[index]
-    // 播放新歌之前, 清楚数据恢复默认值(防止切换有残影)
-    this.setData({ currentSong: {}, sliderValue: 0, currentTime: 0, durationTime: 0 })
-    // 调用函数播放新的歌曲
-    this.setupPlaySong(newSong.id)
-
-    // 将最新的索引保存到共享store中
-    playStore.setState("playSongIndex", index)
+    playStore.dispatch("playNewMusic")
   },
   // 播放模式的监听
   onModeBtnTap() {
-    let modeIndex = this.data.playModeIndex
-    modeIndex ++
-    if (modeIndex === 3) modeIndex = 0
-    this.setData({ 
-      playModeIndex: modeIndex,
-      playModeName: modeNames[modeIndex]
-    })
+    playStore.dispatch("changePlayMode")
   },
 
-  // ---------------------store共享数据---------------------
+  // ---------------------store共享数据处理函数---------------------
   getPlaySongInfosHandler({ playSongIndex, playSongList }) {
     if (playSongIndex !== undefined) {
       this.setData({ playSongIndex })
@@ -243,8 +119,44 @@ Page({
       this.setData({ playSongList })
     }
   },
+  getPlayerInfosHandler({
+    id, currentSong, currentTime, durationTime, lyricInfos, currentLyricText, currentLyricIndex, sliderValue, isPlaying, playModeIndex
+  }) {
+    if (id !== undefined) {
+      this.setData({ id })
+    }
+    if (currentSong) {
+      this.setData({ currentSong })
+    }
+    if (currentTime !== undefined) {
+      this.setData({ currentTime })
+    }
+    if (durationTime !== undefined) {
+      this.setData({ durationTime })
+    }
+    if (lyricInfos) {
+      this.setData({ lyricInfos })
+    }
+    if (currentLyricText) {
+      this.setData({ currentLyricText })
+    }
+    if (currentLyricIndex !== undefined) {
+      this.setData({ currentLyricIndex, lyricScrollTop: currentLyricIndex * 35 })
+    }
+    if (sliderValue !== undefined) {
+      this.setData({ sliderValue })
+    }
+    if (isPlaying !== undefined) {
+      this.setData({ isPlaying })
+    }
+    if (playModeIndex !== undefined) {
+      this.setData({ playModeName: modeNames[playModeIndex] })
+    }
+  },
 
+  // ---------------------取消store中的监听---------------------
   onUnload() {
     playStore.offStates(["playSongList", "playSongIndex"], this.getPlaySongInfosHandler)
+    playStore.offStates(this.data.stateKeys, this.getPlayerInfosHandler)
   }
 })
--- storge/playStore.js
+++ storge/playStore.js
@@ -1,9 +1,163 @@
 import { HYEventStore } from "hy-event-store"
+import { parseLyric } from "../utils/parse-lyric"
+import { getSongDetail, getSongLyric } from "../services/player"
+
+export const audioContext = wx.createInnerAudioContext()
 
 const playStore = new HYEventStore({
   state: {
     playSongList: [],
-    playSongIndex: 0
+    playSongIndex: 0,
+
+    id: 0,
+    currentSong: {},
+    currentTime: 0,
+    durationTime: 0,
+    lyricInfos: [],
+    currentLyricText: "",
+    currentLyricIndex: -1,
+
+    isFirstPlay: true,
+    isPlaying: false,
+
+    sliderValue: 0,
+    playModeIndex: 0
+  },
+  actions: {
+    // 播放歌曲核心逻辑
+    playMusicWithSong(ctx, id) {
+      // 0.播放新歌之前, 恢复数据默认值(防止切歌有残影)
+      ctx.currentSong = {}
+      ctx.sliderValue = 0
+      ctx.currentTime = 0
+      ctx.durationTime = 0
+      ctx.currentLyricIndex = 0
+      ctx.currentLyricText = ""
+      ctx.lyricInfos = []
+
+      // 1.保存id
+      ctx.id = id
+      ctx.isPlaying = true
+  
+      // 2.1根据id获取歌曲的详细数据
+      getSongDetail(id).then(res => {
+        ctx.currentSong = res.songs[0],
+        ctx.durationTime = res.songs[0].dt
+      })
+      // 2.2根据id获取歌曲的歌词数据
+      getSongLyric(id).then(res => {
+        const lyricString = res.lrc.lyric;
+        const lyricInfos = parseLyric(lyricString)
+        ctx.lyricInfos = lyricInfos
+      })
+  
+      // 3.播放当前的歌曲
+      audioContext.stop()
+      audioContext.src = `https://music.163.com/song/media/outer/url?id=${id}.mp3`
+      audioContext.autoplay = true
+  
+      // 第一次进入才需要监听
+      if (ctx.isFirstPlay) {
+        ctx.isFirstPlay = false
+  
+        // 4.监听歌曲播放进度
+        // 4.1调用监听播放进度的函数
+        this.dispatch("timeUpDate")
+  
+        // 4.2当歌曲没有缓存, 正在加载时, 暂停播放
+        audioContext.onWaiting(() => {
+          audioContext.pause()
+        })
+  
+        // 4.3当歌曲缓存完成时, 继续播放
+        audioContext.onCanplay(() => {
+          audioContext.play()
+        })
+  
+        // 4.4监听歌曲播放完成
+        audioContext.onEnded(() => {
+          this.dispatch("playNewMusic")
+        })
+      }
+    },
+    // 监听歌曲实时播放进度
+    timeUpDate(ctx) {
+      audioContext.onTimeUpdate(() => {
+        // 设置当前时间
+        ctx.currentTime = audioContext.currentTime * 1000
+  
+        // 设置滑块进度
+        const sliderValue = ctx.currentTime / ctx.durationTime * 100
+        ctx.sliderValue = sliderValue
+  
+        // 匹配正确歌词
+        if (!ctx.lyricInfos.length) return
+        let index = ctx.lyricInfos.length - 1
+        for (let i = 0; i < ctx.lyricInfos.length; i++) {
+          const info = ctx.lyricInfos[i]
+          if (info.time > audioContext.currentTime * 1000) {
+            index = i - 1
+            break
+          }
+        }
+  
+        // 获取歌词的索引和文本, 并且改变歌词滚动页面的位置
+        if (ctx.currentLyricIndex === index || index === -1) return
+        ctx.currentLyricText = ctx.lyricInfos[index].text
+        ctx.currentLyricIndex = index
+        ctx.lyricScrollTop = 35 * index
+      })
+    },
+    // 改变播放状态
+    changeMusicStatus(ctx) {
+      const isPlaying = audioContext.paused
+      ctx.isPlaying = isPlaying
+      if (isPlaying) {
+        audioContext.play()
+      } else {
+        audioContext.pause()
+      }
+    },
+    // 改变播放模式
+    changePlayMode(ctx) {
+      // 设置新的播放模式
+      let modeIndex = ctx.playModeIndex
+      modeIndex ++
+      if (modeIndex === 3) modeIndex = 0
+
+      // 保存当前播放模式
+      ctx.playModeIndex = modeIndex
+      // ctx.playModeName = modeNames[modeIndex] 
+    },
+    // 上一首下一首的切换
+    playNewMusic(ctx, isNext = true) {
+      // 获取之前的数据
+      let index = ctx.playSongIndex
+      const length = ctx.playSongList.length
+
+      // 根据不同的模式, 将索引重新赋值
+      switch (ctx.playModeIndex) {
+        case 0: // 顺序播放
+          index = isNext ? index + 1 : index - 1
+          if (index === length) index = 0
+          if (index === -1) index = length - 1
+          break;
+        case 1: // 单曲循环
+          break;
+        case 2: // 随机播放
+          index = Math.floor(Math.random() * length)
+          break;
+      }    
+
+      // 根据索引获取歌曲
+      const newSong = ctx.playSongList[index]
+      
+      // 调用函数播放新的歌曲
+      this.dispatch("playMusicWithSong", newSong.id)
+
+      // 将最新的索引保存到共享store中
+      ctx.playSongIndex = index
+    }
   }
 })
 
